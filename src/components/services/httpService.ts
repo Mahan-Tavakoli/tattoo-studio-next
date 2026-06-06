6e6bc58b094d9184dbdd96b0846064e0da85ffb8
kiarash fayaz <kiarash.fayyaz72@outlook.com>
2026-06-06 18:19:24 +0200

    fix: use NEXT_PUBLIC_API_URL env var for API base URL

--- src/components/services/httpService.ts
+++ src/components/services/httpService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "cookies-next";
 
 const BASE_URL =
-  /* process.env.NEXT_PUBLIC_API_URL ?? */ "http://localhost:3100";
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3100";
 
 const app = axios.create({
   baseURL: BASE_URL,
